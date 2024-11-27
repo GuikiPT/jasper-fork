975d1a34be58ddfac4e9de7961beeb07c1f51634
GuikiPT <foxzillapt17@gmail.com>
2024-11-27 18:01:15 -0500

    [FIX] Settings Plugin not containing events specification.

--- Plugins/Settings/SettingsPlugin.ts
+++ Plugins/Settings/SettingsPlugin.ts
@@ -5,5 +5,6 @@ export = DefinePlugin({
     name: "settings",
     description: "Settings you can change in Jasper for your liking.",
     commands: MassRegisterCommands(__dirname, ["Commands"]),
+    events: [],
     public_plugin: true
 }) satisfies Plugin;
