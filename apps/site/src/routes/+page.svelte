2a6e6d140870f7d11b1a960187115aed891290d9
GuikiPT <foxzillapt17@gmail.com>
2025-08-28 11:23:44 +0200

    [FIX] forwarding messages author not specified

--- apps/site/src/routes/+page.svelte
+++ apps/site/src/routes/+page.svelte
@@ -1,30 +1,31 @@
 <script>
-    import parse from "../utils/msgParser.js"
-    import "./app.css"
-    import '@skyra/discord-components-core';
-    
-    let messages = []
+	import parse from '../utils/msgParser.js';
+	import './app.css';
+	import '@skyra/discord-components-core';
 
-    function importPurged(){
-        const input = document.createElement('input');
-        input.type = 'file';
-        input.accept = '.json';
-        input.onchange = async (event) => {
-            const file = event.target.files[0];
-            messages = await parse(file)
-        }
-        input.click();
-    }
+	let messages = [];
+
+	function importPurged() {
+		const input = document.createElement('input');
+		input.type = 'file';
+		input.accept = '.json';
+		input.onchange = async (event) => {
+			const file = event.target.files[0];
+			messages = await parse(file);
+		};
+		input.click();
+	}
 </script>
+
 <button class="button" on:click={() => importPurged()}>Import Messages</button>
 
 <discord-messages>
-{#each messages as [messageid, timestamp, username, id, content, repliedUser, repliedContent]}
-	<discord-message author={`${username} (${id})`} timestamp={timestamp}>
-		{#if repliedUser}
-			<discord-reply slot="reply">{repliedUser}: {repliedContent}</discord-reply>
-		{/if}
-		{content}
-	</discord-message>
-{/each}
+	{#each messages as [messageid, timestamp, username, id, content, repliedUser, repliedContent]}
+		<discord-message author={`${username} (${id})`} {timestamp}>
+			{#if repliedUser}
+				<discord-reply author={repliedUser} slot="reply">{repliedContent}</discord-reply>
+			{/if}
+			{content}
+		</discord-message>
+	{/each}
 </discord-messages>
